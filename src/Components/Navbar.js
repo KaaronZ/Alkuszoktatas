6b1b709e285c79271ad930181e4994c51b53b948
KaaronZ <70701118+KaaronZ@users.noreply.github.com>
2024-07-26 16:14:18 +0200

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import "./CSSComponents/Navbar.css"
+
+function Navbar() {
+  return (
+    <div className="headerTitle">
+      Yello i am Navbar
+      
+    </div>
+  );
+}
+
+export default Navbar;
